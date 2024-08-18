8cd374a8c8e4fe4bb461fa50cb593aea216cce87
Chandu-valasa <168898171+Chandu-valasa@users.noreply.github.com>
2024-08-18 21:54:57 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -81,13 +81,71 @@ resource "aws_security_group" "allow_ssh" {
     Name = "allow_ssh"
   }
 }
+resource "aws_s3_bucket" "mys3" {
+  bucket = "my-tf-test-chandu12"
 
+  tags = {
+    Name        = "My bucket2305"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_iam_role" "ec2_role" {
+  name = "ec2_s3_access_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+resource "aws_iam_policy" "s3_access_policy" {
+  name = "s3_access_policy"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "s3:GetObject",
+          "s3:ListBucket",
+        ]
+        Effect   = "Allow"
+        Resource = [
+          aws_s3_bucket.mys3.arn,
+          "${aws_s3_bucket.mys3.arn}/*"
+        ]
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "s3_access_attachment" {
+  role       = aws_iam_role.ec2_role.name
+  policy_arn = aws_iam_policy.s3_access_policy.arn
+
+}
+  
+  resource "aws_iam_instance_profile" "ec2_instance_profile" {
+  name = "ec2_instance_profile"
+  role = aws_iam_role.ec2_role.name
+}
 resource "aws_instance" "ecc1" {
   ami                    = "ami-0ad21ae1d0696ad58"
   instance_type          = "t2.micro"
   subnet_id              = aws_subnet.mysub1.id
   vpc_security_group_ids = [aws_security_group.allow_ssh.id]
   user_data              = base64encode(file("userdata.sh"))
+
+    iam_instance_profile = aws_iam_instance_profile.ec2_instance_profile.name
 
   tags = {
     Name = "Chandu1"
@@ -114,14 +172,6 @@ output "instance_id_2" {
   value = aws_instance.ecc2.id
 }
 
-resource "aws_s3_bucket" "mys3" {
-  bucket = "my-tf-test-chandu12"
-
-  tags = {
-    Name        = "My bucket2305"
-    Environment = "Dev"
-  }
-}
 
 resource "aws_lb" "test" {
   name               = "test-lb-tf"
